c9528bf6f09ff3aca9b2ff6eaf9395611794ec8c
damian Buń <damian.bun00@gmail.com>
2025-09-13 10:07:35 +0200

    solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,51 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('login');
   });
 
-  it('', () => {
+  it('should allow log in with valid creds', () => {
+    cy.get('#username')
+      .type('tomsmith');
 
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.get('.fa')
+      .click();
+
+    cy.get('#flash')
+      .should('contain.text', 'You logged into a secure area!');
+  });
+
+  it('should not allow log in with invalid creds', () => {
+    cy.get('#username')
+      .type('user123');
+
+    cy.get('#password')
+      .type('passworduser123');
+
+    cy.get('.fa')
+      .click();
+
+    cy.get('#flash')
+      .should('contain.text', 'Your username is invalid!');
+  });
+
+  it('should allow log out from the app', () => {
+    cy.get('#username')
+      .type('tomsmith');
+
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+
+    cy.get('.fa')
+      .click();
+
+    cy.get('.button')
+      .click();
+
+    cy.get('#flash')
+      .should('contain.text', 'You logged out of the secure area!');
   });
 });
